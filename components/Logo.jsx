39e99a8a06521d3cb558a39bb0534adcf651bc16
Hai tran <hai.tran@digitalfortress.dev>
2024-04-22 16:13:50 +0700

    fix: update logo hivello document

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -4,10 +4,10 @@ export function Logo() {
   return (
     <div className="flex items-center gap-4">
       <Image
-        src="/logo-hivello.svg"
+        src="/logo-hivello.png"
         alt="Logo"
-        width={180}
-        height={57}
+        width={104}
+        height={44}
         priority
       />
       <span className="font-bold">Documentation</span>
